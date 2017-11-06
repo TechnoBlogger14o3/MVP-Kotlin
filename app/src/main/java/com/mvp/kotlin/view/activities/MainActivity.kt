a1d1014c8f81f8861033dc430c3df56bf9489d76
TechnoBlogger14o3 <aman.shekhar4@gmail.com>
2017-11-06 11:39:05 +0530

    Nov 6th

---
+++ app/src/main/java/com/mvp/kotlin/view/activities/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.mvp.kotlin.view.activities.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mvp.kotlin.R
+import com.mvp.kotlin.interfaces.MainActivityContract
+import com.mvp.kotlin.presenter.MainActivityPresenter
+import kotlinx.android.synthetic.main.activity_main.*
+
+/***
+ * Created by Techno Blogger on 31/10/17.
+ */
+class MainActivity : AppCompatActivity(), MainActivityContract.View {
+
+    lateinit var presenter: MainActivityPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        presenter = MainActivityPresenter(this)
+    }
+
+    override fun setViewData(data: String) {
+        txtSample!!.text = data
+    }
+
+    override fun initView() {
+        btnSubmit!!.setOnClickListener { v -> presenter.onClick(v) }
+    }
+
+}
